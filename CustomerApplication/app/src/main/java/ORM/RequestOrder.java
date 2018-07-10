09423d53ecd12573396f1ee120371f839c8947b4
nguyenn15 <nguyenn15@student.douglascollege.ca>
2018-07-09 20:23:14 -0700

    lastest code

---
+++ CustomerApplication/app/src/main/java/ORM/RequestOrder.java
@@ -0,0 +1,107 @@
+package ORM;
+
+import java.io.Serializable;
+
+//import javax.persistence.Temporal;
+//import javax.persistence.TemporalType;
+
+public class RequestOrder implements Serializable {
+	
+	public static enum STATUS {PENDING(0), ACCEPTED(1), REJECTED(2);
+	
+		 private final int status;
+		 STATUS(int status) { this.status = status; }
+		 public int getValue() { return status; }
+		 public static STATUS FromInt(int value)
+		 {
+			 switch(value)
+			 {
+				 case 0: return STATUS.PENDING;
+				 case 1: return STATUS.ACCEPTED;
+				 case 2: return STATUS.REJECTED;
+			 }
+			 return STATUS.PENDING;
+		 }
+	}
+	
+	private int idRequest;
+//	@Temporal(TemporalType.TIMESTAMP)
+	private java.util.Date Date;
+	private double Area;
+	private int Medium;
+	private int NoOfDoors;
+	private int Entry_Exit_Doors;
+	private int OpenWindows;
+	private double OpenAreas;
+	private int DoorBell;
+	private String LocationOfService;
+	
+	private int Status = STATUS.PENDING.getValue();
+	private int idCustomer;
+	private int idManager;
+	private int idAdmin;
+	
+	public RequestOrder() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public RequestOrder(int id, double area, int medium, int noofdoors, int entryexitdoors, int openwindows, double openareas, int doorbell, String locationservice, STATUS status, int idcustomer, int idmanager, int idadmin)
+	{
+		this.idRequest= id;
+		this.Area = area;
+		this.Medium= medium;
+		this.NoOfDoors = noofdoors;
+		this.Entry_Exit_Doors= entryexitdoors;
+		this.OpenWindows = openwindows;
+		this.OpenAreas = openareas;
+		this.DoorBell= doorbell;
+		this.LocationOfService = locationservice;
+		this.Status = status.getValue();
+		this.idCustomer = idcustomer;
+		this.idManager = idmanager;
+		this.idAdmin = idadmin;
+		this.Date = new java.util.Date();
+	}
+	
+	public int getIdRequest() { return this.idRequest; }
+	
+	
+	public java.util.Date getDate() { return this.Date; }
+	
+	public double getArea() { return this.Area; }
+	
+	public int getMedium() { return this.Medium; }
+	
+	public int getNoOfDoors() {return this.NoOfDoors;}
+	public int getEntry_Exit_Doors() {return this.Entry_Exit_Doors;}	
+	public int getOpenWindows() {return this.OpenWindows;}
+	public double  getOpenAreas() {return this.OpenAreas;}
+	
+	public int getDoorBell() { return this.DoorBell; }
+	public String getLocationOfService() { return this.LocationOfService; }
+	public int getStatus() { return this.Status; }
+	public int getIdCustomer() { return this.idCustomer; }
+	public int getIdManager() { return this.idManager; }
+	public int getIdAdmin() { return this.idAdmin; }
+	
+	public void setIdRequest(int idrequest) {  this.idRequest = idrequest; }
+	
+	
+	public void setDate(java.util.Date date) {  this.Date = date; }
+	
+	public void setArea(double area) {  this.Area = area; }
+	
+	public void setMedium(int medium) {  this.Medium = medium; }
+	
+	public void setNoOfDoors(int doors) { this.NoOfDoors= doors;}
+	public void setEntry_Exit_Doors(int entries) { this.Entry_Exit_Doors = entries;}	
+	public void setOpenWindows(int openwindows) { this.OpenWindows = openwindows;}
+	public void  setOpenAreas(double area) { this.OpenAreas = area;}
+	
+	public void setDoorBell(int doorbell) {  this.DoorBell= doorbell; }
+	public void setLocationOfService(String locationservice) {  this.LocationOfService = locationservice; }
+	public void setIdCustomer(int idcustomer) {  this.idCustomer = idcustomer; }
+	public void setIdManager(int idmanager) {  this.idManager= idmanager; }
+	public void setIdAdmin(int idAdmin) {this.idAdmin = idAdmin; }
+
+}
--- CustomerApplication/app/src/main/java/com/example/amandeep/customerapplication/LoginActivity.java
+++ CustomerApplication/app/src/main/java/com/example/amandeep/customerapplication/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.amandeep.customerapplication;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,16 +11,35 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import ORM.User;
+import ORM.UserType;
+import Service.UserApi;
+import Service.UserService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
     Button login;
-    EditText email,password;
+    EditText email;
+    EditText password;
     List<String> loginCustomer=new ArrayList<>();
 
+    // service for login
+    UserService myUserService;
+
+    User user;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)   {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
@@ -25,18 +47,31 @@ public class LoginActivity extends AppCompatActivity {
         email=(EditText)findViewById(R.id.userId);
         password=(EditText)findViewById(R.id.password);
 
-        loginCustomer.add("arsh");
-        loginCustomer.add("12345");
+        //loginCustomer.add("arsh");
+        //loginCustomer.add("12345");
+
+        myUserService = new UserService();
+        //
+
+        users();
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(email.getText().toString().equals(loginCustomer.get(0))
-                        &&password.getText().toString().equals(loginCustomer.get(1)))
+
+                final String uname = email.getText().toString();
+                final String pass = password.getText().toString();
+                final String usertype = "CUSTOMER"; // this app is for customer
+
+                User user =new User(); //myUserService.login(uname,pass,usertype);
+                // create async task
+
+
+                if(user!=null)
                 {
                     Intent myIntent = new Intent(LoginActivity.this,
                             CustomerPage.class);
-                    startActivity(myIntent);
+                   // startActivity(myIntent);
                 }
                 else
                 {
@@ -47,4 +82,63 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    public void Login()
+    {
+        final String uname = email.getText().toString();
+        final String pass = password.getText().toString();
+        final String usertype = "CUSTOMER"; // this app is for customer
+
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("username", uname);
+        params.put("password", pass);
+        params.put("usertype", usertype); /// ADMIN, CUSTOMER, EMPLOYEE, MANAGER
+
+        try {
+            UserApi userapi = myUserService.GetUserApi();
+            Call<User> users = userapi.Login(params);
+            users.enqueue(new Callback<User>() {
+                @Override
+                public void onResponse(Call<User> call, Response<User> response) {
+                    // login succes if id >0
+                    user = response.body();
+                    if()
+                }
+
+                @Override
+                public void onFailure(Call<User> call, Throwable t) {
+
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void users()
+    {
+        try {
+            UserApi userapi = myUserService.GetUserApi();
+            Call<List<User>> users = userapi.Users();
+            users.enqueue(new Callback<List<User>>() {
+                @Override
+                public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                    int i=0;
+                }
+
+                @Override
+                public void onFailure(Call<List<User>> call, Throwable t) {
+
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
