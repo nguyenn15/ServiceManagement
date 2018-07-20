3c9dc3b617d2180f377f8540676d9daa0227b8c1
Kaur <kaurr49@student.douglascollege.ca>
2018-07-19 20:25:20 -0700

    Added SendQuoteToCustomer Page

---
+++ Admin/app/src/main/java/com/example/a300273215/admin/SendQuoteToCustomer.java
@@ -0,0 +1,13 @@
+package com.example.a300273215.admin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SendQuoteToCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_quote_to_customer);
+    }
+}
